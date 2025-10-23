fd2e31fea6c9eff0f2536d02856d50ea61857bdd
miguelbraga04 <160123531+miguelbraga04@users.noreply.github.com>
2025-10-23 09:43:40 +0300

    Snake Game added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //const { Message } = require("./paho-mqtt.min");
 
 // Pick a random div by ID
-const divIds = ["case1", "case2", "case3", "case4"];
+const divIds = ["case1", "case2", "case3", "case4", "case5"];
 const randomId = divIds[Math.floor(Math.random() * divIds.length)];
 console.log(randomId);
 const selectedDiv = document.getElementById(randomId);
@@ -337,39 +337,249 @@ switch (randomId) {
     
     case "case4":
         document.getElementById('title').innerHTML = "4";
-        document.getElementById("checkForm").addEventListener("submit", function(event){
+        document.getElementById("case4Form").addEventListener("submit", function(event){
             event.preventDefault(); // prevent form from refreshing the page
             if (client.isConnected() === true) {
                 publishMessage();
+                document.getElementById("case4_msg").textContent = "Box unlocked"
             } else {
-                document.getElementById("case4_msg").textContent = "Odotetaan MQTT yhteyttä. Yritä uudelleen."
+                document.getElementById("case4_msg").textContent = "Waiting for MQTT connection before sending message. Try again."
                 /*console.log("Waiting for MQTT connection before sending message. Try again.")*/
             }
         });
-        document.getElementById("case4_msg").textContent = "Laatikko avattu"
         /*selectedDiv.textContent = "Mysterybox has been unlocked!";*/
         break;
-}
 
-function onConnect() {
-    console.log("Connected to MQTT broker");
-    // Now it's safe to publish
-}
+    case "case5":
+        document.getElementById('title').innerHTML = "5";
 
-function isConnected() {
-    return client && client.isConnected();
-}
+        let canvas = document.getElementById("game");
+        let ctx = canvas.getContext("2d");  
+        // draw on the screen to get the context, ask canvas  to get the 2d context
 
-function publishMessage(){
-    msg = "UNLOCK"
-    topic = "mysterybox";
-    Message = new Paho.Message(msg);
-    Message.destinationName = topic;
-    //client.send(Message);
-    if (client.isConnected()) {
-        client.publish(topic, msg);
-        console.log("Message \"" +msg+ "\" to topic \"" +topic+ "\" is sent");
-    } else {
-        console.log("MQTT client not connected yet.");
-    }
+        // snake axis
+        class SnakePart {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        }
+        // speed of the game
+        let speed = 7;
+        // size and count of a tile 
+        let tileCount = 20;
+        let tileSize = canvas.width / tileCount - 2;
+        // head of the snake
+        let headX = 10;
+        let headY = 10;
+        let snakeParts = [];
+        let tailLength = 2;
+        // apple size
+        let appleX = 5;
+        let appleY = 5;
+        // movement
+        let inputsXVelocity = 0;
+        let inputsYVelocity = 0;
+
+        let xVelocity = 0;
+        let yVelocity = 0;
+
+        let snakeScore = 0;
+
+        /*let gulpSound = new Audio("gulp.mp3");*/
+
+        //game loop
+        function drawGame() {
+        xVelocity = inputsXVelocity;
+        yVelocity = inputsYVelocity;
+
+        changeSnakePosition();
+        let result = isGameOver();
+        if (result) {
+            return;
+        }
+
+        clearScreen();
+
+        checkAppleCollision();
+        drawApple();
+        drawSnake();
+
+        drawScore();
+
+        //score 5 to send unlock message
+        if (snakeScore >= 5) {
+            publishMessage();
+            document.getElementById("case5_msg").textContent = "Box unlocked"
+        }
+
+        if (snakeScore > 5) {
+            speed = 9;
+        }
+        if (snakeScore > 10) {
+            speed = 11;
+        }
+
+        setTimeout(drawGame, 1000 / speed);
+        }
+
+        function isGameOver() {
+        let gameOver = false;
+
+        if (yVelocity === 0 && xVelocity === 0) {
+            return false;
+        }
+
+        //walls
+        if (headX < 0) {
+            gameOver = true;
+        } else if (headX === tileCount) {
+            gameOver = true;
+        } else if (headY < 0) {
+            gameOver = true;
+        } else if (headY === tileCount) {
+            gameOver = true;
+        }
+
+        for (let i = 0; i < snakeParts.length; i++) {
+            let part = snakeParts[i];
+            if (part.x === headX && part.y === headY) {
+            gameOver = true;
+            break;
+            }
+        }
+
+        if (gameOver) {
+            ctx.fillStyle = "white";
+            ctx.font = "50px Verdana";
+
+            if (gameOver) {
+            ctx.fillStyle = "white";
+            ctx.font = "50px Verdana";
+
+            var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+            gradient.addColorStop("0", " magenta");
+            gradient.addColorStop("0.5", "blue");
+            gradient.addColorStop("1.0", "red");
+            // Fill with gradient
+            ctx.fillStyle = gradient;
+
+            ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+            }
+
+            ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+        }
+
+        return gameOver;
+
+        }
+
+        function drawScore() {
+        ctx.fillStyle = "white";
+        ctx.font = "10px Verdana";
+        ctx.fillText("Score " + snakeScore, canvas.width - 50, 10);
+        }
+
+        function clearScreen() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
+        function drawSnake() {
+        ctx.fillStyle = "green";
+        for (let i = 0; i < snakeParts.length; i++) {
+            let part = snakeParts[i];
+            ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+        }
+
+        snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+        while (snakeParts.length > tailLength) {
+            snakeParts.shift(); // remove the furthet item from the snake parts if have more than our tail size.
+        }
+
+        ctx.fillStyle = "orange";
+        ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+        }
+
+        function changeSnakePosition() {
+        headX = headX + xVelocity;
+        headY = headY + yVelocity;
+        }
+
+        function drawApple() {
+        ctx.fillStyle = "red";
+        ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+        }
+
+        function checkAppleCollision() {
+        if (appleX === headX && appleY == headY) {
+            appleX = Math.floor(Math.random() * tileCount);
+            appleY = Math.floor(Math.random() * tileCount);
+            tailLength++;
+            snakeScore++;
+            /*gulpSound.play();*/
+        }
+        }
+
+        document.body.addEventListener("keydown", keyDown);
+
+        function keyDown(event) {
+        //up
+        if (event.keyCode == 38 || event.keyCode == 87) {
+            //87 is w
+            if (inputsYVelocity == 1) return;
+            inputsYVelocity = -1;
+            inputsXVelocity = 0;
+        }
+
+        //down
+        if (event.keyCode == 40 || event.keyCode == 83) {
+            // 83 is s
+            if (inputsYVelocity == -1) return;
+            inputsYVelocity = 1;
+            inputsXVelocity = 0;
+        }
+
+        //left
+        if (event.keyCode == 37 || event.keyCode == 65) {
+            // 65 is a
+            if (inputsXVelocity == 1) return;
+            inputsYVelocity = 0;
+            inputsXVelocity = -1;
+        }
+
+        //right
+        if (event.keyCode == 39 || event.keyCode == 68) {
+            //68 is d
+            if (inputsXVelocity == -1) return;
+            inputsYVelocity = 0;
+            inputsXVelocity = 1;
+        }
+        }
+
+        drawGame();
+                break;
+        }
+
+        function onConnect() {
+            console.log("Connected to MQTT broker");
+            // Now it's safe to publish
+        }
+
+        function isConnected() {
+            return client && client.isConnected();
+        }
+
+        function publishMessage(){
+            msg = "UNLOCK"
+            topic = "mysterybox";
+            Message = new Paho.Message(msg);
+            Message.destinationName = topic;
+            //client.send(Message);
+            if (client.isConnected()) {
+                client.publish(topic, msg);
+                console.log("Message \"" +msg+ "\" to topic \"" +topic+ "\" is sent");
+            } else {
+                console.log("MQTT client not connected yet.");
+            }
 }
